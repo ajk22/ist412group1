19c17279697596647b9c043ec722a6e96f72ecec
Michael Cao <mike.x.cao@gmail.com>
2021-10-03 23:53:06 -0400

    Update userPlanner.java

--- src/main/java/planning/userPlanner.java
+++ src/main/java/planning/userPlanner.java
@@ -6,8 +6,9 @@
 package planning;
 
 /**
- *
- * @author alexkinser
+ * This class will assist in the display of plans
+ * for the user by providing an object or list that
+ * interacts with the plan and planList objects.
  */
 public class userPlanner {
     

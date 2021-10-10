f20357cf8717851bf80f04569f8b414cc44d0a73
nzp126 <55207429+nzp126@users.noreply.github.com>
2021-10-10 14:33:32 -0400

    Add files via upload
    New Plan class

--- src/main/java/planning/plan.java
+++ src/main/java/planning/plan.java
@@ -5,6 +5,7 @@
  */
 package planning;
 
+
 /**
  * This abstract class will represent a "plan" that a user makes, possibly
  * including an attraction, the day planned, time planned, distance, cost
@@ -12,6 +13,38 @@ package planning;
  */
 public class plan {
     
+    String attraction;
+    String dayPlanned;
+    String timePlanned;
+    Double cost;
+    
+    public plan(String attraction, String dayPlanned, String timePlanned, Double cost){
+        this.attraction = attraction;
+        this.dayPlanned = dayPlanned;
+        this.timePlanned = timePlanned;
+        this.cost = cost;
+    }
     //an object
     //a plan should have an attraction, day planned, time planned, estimated travel distance, etc...
+    
+    public void setDayPlanned(){
+    }
+    
+    public void setTimePlanned(){
+    }
+    
+    public void setAttraction(){
+    }
+    
+    public void setCost(){
+    }
+    
+    public Double getCost(){
+        return cost;
+    }
+    public String getAttraction() {
+        return attraction;
+    }
+    
+    
 }
